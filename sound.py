04652c401836e5feb22607cc0542ae30f4e5e6a8
Jorik Tuinman <joriktuinman@hotmail.com>
2022-06-26 20:36:24 +0200

    .

--- sound.py
+++ sound.py
@@ -37,8 +37,20 @@ def play_hint(soundpath):
     length = hint.get_length()
     logger.info("Length of sound bit is "+ str(int(length)) + " seconds.")
     hint.get_volume()
+
+## Before playing, lower the volume of the music
+    if pygame.mixer.music.get_busy():
+        if(pygame.mixer.music.get_volume() > 0.2):
+            pygame.mixer.music.set_volume(0.2)
+            logger.info("Volume down to "+ str(int(pygame.mixer.music.get_volume())))
+        else:
+            pygame.mixer.music.set_volume(0.0)
     pygame.mixer.Channel(2).play(hint)
 
+    logger.info("Playing "+ soundpath)
+    if pygame.mixer.music.get_busy():
+        time.sleep(length +1)
+        pygame.mixer.music.set_volume(float(music_volume) / 100)
     
 def stop_hint():
     fade = config.getint("Escape","fadeout")
