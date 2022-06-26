55ec146df9b9a9a50062190a804c1252c5ec018b
Jorik Tuinman <joriktuinman@hotmail.com>
2022-06-26 20:37:19 +0200

    .

--- sound.py
+++ sound.py
@@ -151,6 +151,6 @@ sound_volume = config.getfloat("Escape", "sound_volume")
 pygame.mixer.music.set_volume(music_volume / 100)
 
 logger.info("Number of channels: " + str(pygame.mixer.get_num_channels()))
-play_hint(sounddir + config.get("Escape","music_state_state1"))
-play_music(sounddir + config.get("Escape","music_state_state2"))
 
+play_music(sounddir + config.get("Escape","music_state_state2"))
+play_hint(sounddir + config.get("Escape","music_state_state1"))
