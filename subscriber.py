33a73c6eeb0c634bfd42447f7c10cb5851ed8c12
Jorik Tuinman <joriktuinman@hotmail.com>
2022-06-29 19:28:07 +0200

    .

--- subscriber.py
+++ subscriber.py
@@ -4,23 +4,33 @@ import signal
 import sys
 
 def signal_handler(sig, frame):
-    clientSubscribe.loop_stop()
+    client.loop_stop()
     print("Exiting")
     sys.exit(0)
 
+def on_connect(client, userdata, flags, rc):
+    print("connected with resultcode " + str(rc))
+    client.subscribe("SOUNDMACHINE/+") 
+
 def on_message(client, userdata, message):
     print("received message: " ,str(message.payload.decode("utf-8")))
-
+    if message.topic == "SOUNDMACHINE/HINTS":
+        with open('home/pi/mqtt_update.txt', 'a+') as f:
+            f.write("received topic2")
 
 signal.signal(signal.SIGINT, signal_handler)
-clientSubscribe = mqtt.Client("Mixer")
-clientSubscribe.connect("192.168.178.30") 
 
-clientSubscribe.loop_start()
-clientSubscribe.unsubscribe("#")
-clientSubscribe.subscribe("SOUNDMACHINE/+")
+broker_address = "192.168.178.30"  # Broker address
+port = 1883  # Broker port
+# user = "yourUser"                    #Connection username
+# password = "yourPassword"            #Connection password
 
-clientSubscribe.on_message = on_message 
- 
-time.sleep(60)
-clientSubscribe.loop_stop()
+client = mqtt.Client()  # create new instance
+# client.username_pw_set(user, password=password)    #set username and password
+client.on_connect = on_connect  # attach function to callback
+client.on_message = on_message  # attach function to callback
+
+client.connect(broker_address, port=port)  # connect to broker
+
+client.loop_forever()
+
